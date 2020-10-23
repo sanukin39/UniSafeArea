fbcace34b9eb23f6f3a1aa6ea2bc11ea3e3803c7
sanukin39 <ericsson.goodjob@gmail.com>
2020-10-23 18:36:45 +0900

    Inplement base functions

---
+++ Assets/UniSafeArea/Scripts/SafeAreaProvider.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+namespace UniSafeArea
+{
+    public static class SafeAreaProvider
+    {
+        public static Rect GetSafeArea()
+        {
+            var safeArea = Screen.safeArea;
+#if !UNITY_EDITOR
+            return safeArea;
+#endif
+            var width = Screen.width;
+            var height = Screen.height;
+            var isPortrait = height > width;
+
+            // iPhone X/XS
+            var device = new DeviceSafeAreaMargin(1125, 2436, new ScreenMargin(132, 102, 0, 0), new ScreenMargin(0, 63, 132, 132));
+            if (HasSaveResolution(device, width, height))
+            {
+                return CalcSafeArea(device, isPortrait);
+            }
+            
+            // iPhone XR
+            device = new DeviceSafeAreaMargin(828, 1792, new ScreenMargin(88, 68, 0, 0), new ScreenMargin(0, 42, 88, 88));
+            if (HasSaveResolution(device, width, height))
+            {
+                return CalcSafeArea(device, isPortrait);
+            }
+
+            return safeArea;
+        }
+
+        static bool HasSaveResolution(DeviceSafeAreaMargin device, int width, int height)
+        {
+            return device.ScreenWidth == width && device.ScreenHeight == height ||
+                   device.ScreenHeight == width && device.ScreenWidth == height;
+        }
+
+        static Rect CalcSafeArea(DeviceSafeAreaMargin device, bool isPortrait)
+        {
+            var margin = isPortrait ? device.PortraitMargin : device.LandscapeMargin;
+            var width = isPortrait ? device.ScreenWidth : device.ScreenHeight;
+            var height = isPortrait ? device.ScreenHeight : device.ScreenWidth;
+            return Rect.MinMaxRect(margin.Left, margin.Bottom, width - margin.Right, height - margin.Top);
+        }
+
+        readonly struct DeviceSafeAreaMargin
+        {
+            public readonly int ScreenWidth;
+            public readonly int ScreenHeight;
+            public readonly ScreenMargin PortraitMargin;
+            public readonly ScreenMargin LandscapeMargin;
+
+            public DeviceSafeAreaMargin(int screenWidth, int screenHeight, ScreenMargin portraitMargin, ScreenMargin landscapeMargin)
+            {
+                ScreenWidth = screenWidth;
+                ScreenHeight = screenHeight;
+                PortraitMargin = portraitMargin;
+                LandscapeMargin = landscapeMargin;
+            }
+        }
+
+        readonly struct ScreenMargin
+        {
+            public readonly int Top;
+            public readonly int Bottom;
+            public readonly int Left;
+            public readonly int Right;
+
+            public ScreenMargin(int top, int bottom, int left, int right)
+            {
+                Top = top;
+                Bottom = bottom;
+                Left = left;
+                Right = right;
+            }
+        }
+    }
+}
---
+++ Assets/UniSafeArea/Scripts/UniSafeArea.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace UniSafeArea
+{
+    [ExecuteInEditMode]
+    public class UniSafeArea : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _safeAreaTransform = null;
+        private Resolution _resolutionCache;
+
+        private void Update()
+        {
+            if (_safeAreaTransform == null || _resolutionCache.Equals(Screen.resolutions))
+            {
+                return;
+            }
+
+            UpdateSafeArea();
+        }
+
+        void UpdateSafeArea()
+        {
+            var area = SafeAreaProvider.GetSafeArea();
+            var resolution = Screen.currentResolution;
+            var rect = _safeAreaTransform;
+            rect.anchorMax = new Vector2(area.xMax / Screen.width, area.yMax / Screen.height);
+            rect.anchorMin = new Vector2(area.xMin / Screen.width, area.yMin / Screen.height);
+            _resolutionCache = resolution;
+        } 
+    }
+}
