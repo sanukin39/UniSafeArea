347043bf9a8073d11b06415b4d763414f9032acd
sanukin39 <sanukiwataru@sanukikounoMac-mini.local>
2020-10-24 09:37:48 +0900

    Add example scene

---
+++ Assets/Example/SafeAreaText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UniSafeArea.Sample
+{
+    [RequireComponent(typeof(Text))]
+    public class SafeAreaText : MonoBehaviour
+    {
+        private Text _text;
+        
+        void Awake()
+        {
+            _text = GetComponent<Text>();
+        }
+
+        void Update()
+        {
+            ApplySafeAreaData(_text);
+        }
+
+        void ApplySafeAreaData(Text text)
+        {
+            var safeArea = SafeAreaProvider.GetSafeArea();
+            text.text = $"Screen Size {Screen.width} : {Screen.height} {Environment.NewLine} Safe Area {safeArea} {Environment.NewLine}"
+                        + $"Margin Left {safeArea.x} : Bottom {safeArea.y} : Right {Screen.width - safeArea.width - safeArea.x} : Top {Screen.height - safeArea.height - safeArea.y}";
+        }
+    }
+}
