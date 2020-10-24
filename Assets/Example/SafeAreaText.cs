765509bd9e42bbcc18241505029343c8a1695b92
sanukin39 <sanukiwataru@sanukikounoMac-mini.local>
2020-10-24 11:24:44 +0900

    Edit example text

--- Assets/Example/SafeAreaText.cs
+++ Assets/Example/SafeAreaText.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -22,8 +22,17 @@ namespace UniSafeArea.Sample
         void ApplySafeAreaData(Text text)
         {
             var safeArea = SafeAreaProvider.GetSafeArea();
-            text.text = $"Screen Size {Screen.width} : {Screen.height} {Environment.NewLine} Safe Area {safeArea} {Environment.NewLine}"
-                        + $"Margin Left {safeArea.x} : Bottom {safeArea.y} : Right {Screen.width - safeArea.width - safeArea.x} : Top {Screen.height - safeArea.height - safeArea.y}";
+            var strBuilder = new StringBuilder();
+            strBuilder.AppendLine("Screen Size");
+            strBuilder.AppendLine($"{Screen.width} : {Screen.height}");
+            strBuilder.AppendLine();
+            strBuilder.AppendLine("Safe Area");
+            strBuilder.AppendLine(safeArea.ToString());
+            strBuilder.AppendLine();
+            strBuilder.AppendLine("Margin");
+            strBuilder.AppendLine(
+                $"Left {safeArea.x} : Bottom {safeArea.y} : Right {Screen.width - safeArea.width - safeArea.x} : Top {Screen.height - safeArea.height - safeArea.y}");
+            text.text = strBuilder.ToString();
         }
     }
 }
